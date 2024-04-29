fed760a8c2be2bb2bf8c45746ef652720405f763
pollyndos <56310766+pollyndos@users.noreply.github.com>
2024-04-29 12:50:05 +0400

    add regions to Form1.cs

--- CS/RuntimeSqlDataSourceReportSample/Form1.cs
+++ CS/RuntimeSqlDataSourceReportSample/Form1.cs
@@ -1,16 +1,22 @@
+﻿#region usingsSql
 using DevExpress.DataAccess.Sql;
 using DevExpress.DataAccess.ConnectionParameters;
+#endregion
+#region usings
 using DevExpress.DataAccess.Wizard.Services;
 using DevExpress.XtraReports.UI;
 using DevExpress.XtraReports.UserDesigner;
 using System.ComponentModel.Design;
 using System;
+#endregion
 
 namespace RuntimeSqlDataSourceReportSample
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        #region datasource
         SqlDataSource DataSource { get; set; }
+        #endregion
         public Form1()
         {
             InitializeComponent();
@@ -23,12 +29,15 @@ namespace RuntimeSqlDataSourceReportSample
 
         private void OpenReportDesigner()
         {
+            #region OpenReportDesigner
             CreateSqlDataSource();
             XtraReport rep = ReportCreator.CreateReport(DataSource);
             ReportDesignTool designer = new ReportDesignTool(rep);
             designer.ShowRibbonDesignerDialog();
+            #endregion
         }
 
+        #region CreateSqlDataSource
         public void CreateSqlDataSource()
         {
             InitializeSqlDataSource();
@@ -40,7 +49,9 @@ namespace RuntimeSqlDataSourceReportSample
 
             DataSource.RebuildResultSchema();
         }
+        #endregion
 
+        #region InitializeSqlDataSource
         void InitializeSqlDataSource()
         {
             //CreateConnectionFromParameters();
@@ -51,6 +62,8 @@ namespace RuntimeSqlDataSourceReportSample
 
             //UseConfigureDataConnectionEvent();
         }
+        #endregion
+        #region CreateConnectionFromParameters
         private void CreateConnectionFromParameters()
         {
             MsSqlConnectionParameters connectionParameters = new MsSqlConnectionParameters()
@@ -63,10 +76,14 @@ namespace RuntimeSqlDataSourceReportSample
             };
             DataSource = new SqlDataSource(connectionParameters);
         }
+        #endregion
+        #region CreateConnectionFromAppConfig
         private void CreateConnectionFromAppConfig()
         {
             DataSource = new SqlDataSource("TestConnectionString");
         }
+        #endregion
+        #region CreateConnectionFromString
         private void CreateConnectionFromString()
         {
             string connectionString = "XpoProvider=MSSqlServer;Data Source=(LocalDB)\\MSSQLLocalDB;" +
@@ -76,7 +93,9 @@ namespace RuntimeSqlDataSourceReportSample
                 new CustomStringConnectionParameters(connectionString);
             DataSource = new SqlDataSource(connectionParameters);
         }
+        #endregion
 
+        #region AddRelations
         void AddQueryRelations()
         {
             SelectQuery categories = SelectQueryFluentBuilder
@@ -92,10 +111,12 @@ namespace RuntimeSqlDataSourceReportSample
             DataSource.Relations.Add(
                 new MasterDetailInfo("Categories", "Products", "CategoryID", "CategoryID"));
         }
+        #endregion
         private void simpleButton2_Click(object sender, EventArgs e)
         {
             OpenReportDesignerWithService();
         }
+        #region OpenReportDesignerWithService
         private void OpenReportDesignerWithService()
         {
             CustomConnectionProviderService connectionProviderService =
@@ -126,5 +147,6 @@ namespace RuntimeSqlDataSourceReportSample
             ReplaceService(e.DesignerHost, typeof(IConnectionProviderService),
                 new CustomConnectionProviderService());
         }
+        #endregion
     }
 }
